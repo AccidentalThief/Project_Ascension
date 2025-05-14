b80dd9eba7b5853e7140c531151948d5f585a3e1
AccidentalThief <baisleymaxon@gmail.com>
2025-05-14 02:23:33 -0400

    Update CameraMovement.cs
    Added Mouse Sensitivity

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,13 +5,14 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] Vector3 followDistance;
+    [SerializeField] float mouseSensitivity = 2f;
 
     float rotationY;
     //float rotationX;
 
     void Update()
     {
-        rotationY += Input.GetAxis("Mouse X");
+        rotationY += Input.GetAxis("Mouse X") * mouseSensitivity;
 
         var targetRotation = Quaternion.Euler(16.89f, rotationY, 0f);
         
