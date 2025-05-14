d04b4786a4ef11fb73a5ed444fbabd9a17e33ee1
AccidentalThief <baisleymaxon@gmail.com>
2025-05-14 08:23:19 -0400

    Added Up and Down looking with bounds

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,13 +8,25 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] float mouseSensitivity = 2f;
 
     float rotationY;
+    float rotationX = 16.9f;
     //float rotationX;
 
     void Update()
     {
+        rotationX += Input.GetAxis("Mouse Y") * -mouseSensitivity;
         rotationY += Input.GetAxis("Mouse X") * mouseSensitivity;
 
-        var targetRotation = Quaternion.Euler(16.89f, rotationY, 0f);
+        if (rotationX > 360)
+            rotationX -= 360;
+        if (rotationY > 360)
+            rotationY -= 360;
+
+        if (rotationX > 60f)
+            rotationX = 60f;
+        if (rotationX < -60f)
+            rotationX = -60f;
+
+        var targetRotation = Quaternion.Euler(rotationX, rotationY, 0f);
         
         transform.position = target.position + targetRotation * followDistance;
         transform.rotation = targetRotation;
