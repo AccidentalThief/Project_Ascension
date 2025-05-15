0c7133f2c24615e08732ce10cf0b97c8f41e227e
AccidentalThief <baisleymaxon@gmail.com>
2025-05-15 12:54:03 -0400

    Shooting into the sky works

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float speed;
+    
     void Start()
     {
         RaycastHit hit;
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
         if (Physics.Raycast(ray, out hit)) {
             transform.LookAt(hit.point);
+        }
+        else {
+            Vector3 altTarget = ray.direction * 100;
+            transform.LookAt(altTarget);
         }
     }
 
